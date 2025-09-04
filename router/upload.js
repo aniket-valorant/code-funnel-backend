ae96e8ae7553773b5665f993720ddfbaa402accb
aniket1137 <waniket011@gmail.com>
2025-09-04 10:47:58 +0530

    Add streamifier for image upload handling and improve error responses

--- router/upload.js
+++ router/upload.js
@@ -1,22 +1,34 @@
 const express = require ("express");
 const multer = require ("multer");
 const cloudinary = require ("../config/cloudinary.js");
-
+const streamifier = require("streamifier");
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({storage});
 
-// POST /api/upload
 router.post("/", upload.single("image"), async (req, res) => {
   try {
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: "codes", // optional folder
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    const result = await new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        { folder: "codes" },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        }
+      );
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
     });
+
     res.json({ url: result.secure_url, public_id: result.public_id });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Upload failed" });
   }
 });
+
 
 module.exports = router;
